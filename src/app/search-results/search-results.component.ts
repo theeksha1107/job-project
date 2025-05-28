19f51bedc09045046c18fffe3be6119dc702e78f
Thiyagarajan Vellaichamy <theekshareddy11@gmail.com>
2025-05-28 14:41:35 +0530

    code modified

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
+  standalone: true,
   imports: [CommonModule,RouterModule],
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css'],
